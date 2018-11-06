29fa49e23edd6517afeee7740decae291568af02
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-11-06 19:32:37 +0600

    Add files via upload

---
+++ 5.11/ConsoleApp3/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Person
+    {
+        public string FullName { get; set; }      
+        public int Age { get; set; }
+    }
+}
---
+++ 5.11/ConsoleApp3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+            new Person
+            {
+               FullName = "Admin Admin",
+               Age = 35
+               },
+
+            new Person
+            {
+               FullName = "Admin Admin",
+               Age = 35
+               }              
+            };
+
+
+            List<Mother> car = new List<Mother>
+            {
+            new Mother
+            {
+               FullName=  "Admin Admin",
+               Age = 221
+               },
+
+                new Mother
+               {
+                FullName=  "Admin Admin",
+               Age = 221
+               }
+            };
+
+
+            List<Father> home = new List<Father>
+            {
+            new Father
+            {
+               FullName=  "Admin Admin",
+               Age = 132
+               },
+
+                new Father
+               {
+                FullName=  "Admin Admin",
+               Age = 132
+               }
+            };
+            Console.ReadKey();
+        }
+    }
+}
