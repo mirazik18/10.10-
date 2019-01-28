06ffc933e20536ed76eb9fa2593bd75cb733b64d
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2019-01-28 19:07:52 +0600

    Add files via upload

---
+++ SportEFHW/SportEFHW/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportEFHW
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Number { get; set; }
+        
+        public int? TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
---
+++ SportEFHW/SportEFHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportEFHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new Sportcontext())
+            {
+
+                Team team = new Team
+                {
+                    Name = "PSG"
+                };
+                context.Teams.Add(team);
+                Player playerNeymar = new Player
+                {
+                    FullName = "Neymar",
+                    Number = 4,
+                    Team = team
+                    
+                };
+                context.Players.Add(playerNeymar);
+                context.SaveChanges();
+
+                var players = from Players in context.Players
+                              where Players.FullName == "Neymar"
+                              select Players;
+
+                Player player = players.FirstOrDefault();
+
+                Console.WriteLine("Имя: " + player.FullName + "Number: " + player.Number + "Id Team: " + player.TeamId);
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
